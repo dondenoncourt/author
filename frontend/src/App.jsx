ec4fe28e87c20ee14c2a7017c9e59f7e1d38be2c
Don Denoncourt <don.denoncourt@litera.com>
2023-09-07 07:19:37 -0500

    scrollable pane

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,35 +3,64 @@ import testBoard from './shodan-test-board.jpg';
 import './App.css';
 import Skills from './skills.js';
 import Subscribers from './subscribers.js';
+import { getTheme, mergeStyleSets } from '@fluentui/react'
 import {
-  Accordion,
-  AccordionHeader,
-  AccordionItem,
-  AccordionPanel,
+  ThemeProvider, initializeIcons
 } from "@fluentui/react-components";
+import { ScrollablePane, IScrollablePaneStyles } from '@fluentui/react/lib/ScrollablePane';
+import { Sticky, StickyPositionType } from '@fluentui/react/lib/Sticky';
 
+const theme = getTheme();
+const classNames = mergeStyleSets({
+  wrapper: {
+    height: '40vh',
+    position: 'relative',
+    maxHeight: 'inherit',
+  },
+  pane: {
+    width: '100%',
+    border: '1px solid ' + theme.palette.neutralLight,
+  },
+  sticky: {
+    color: theme.palette.neutralDark,
+    padding: '5px 20px 5px 10px',
+    fontSize: '13px',
+    borderTop: '1px solid ' + theme.palette.black,
+    borderBottom: '1px solid ' + theme.palette.black,
+  },
+  textContent: {
+    padding: '15px 10px',
+  },
+});
+const scrollablePaneStyles: Partial<IScrollablePaneStyles> = { root: classNames.pane };
 
 function App() {
-  return (
+ return (
     <React.Fragment>
       <div className="App">
         <header className="App-header">
           <img src={testBoard} className="shodan-logo" alt="logo" />
         </header>
-        <Accordion>
-          <AccordionItem value="1">
-            <AccordionHeader>Subscribers</AccordionHeader>
-            <AccordionPanel>
-              <Subscribers/>
-            </AccordionPanel>
-          </AccordionItem>
-          <AccordionItem value="2">
-            <AccordionHeader>Skills</AccordionHeader>
-            <AccordionPanel>
-              <Skills/>
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
+        <div className={classNames.wrapper}>
+          <ScrollablePane
+            scrollContainerFocus={true}
+            scrollContainerAriaLabel="Sticky component example"
+            styles={scrollablePaneStyles}
+          >
+            <Sticky stickyPosition={StickyPositionType.Both}>
+              <div role="heading" aria-level={1} className={classNames.sticky}>
+                Subsribers
+              </div>
+            </Sticky>
+            <Subscribers/>
+            <Sticky stickyPosition={StickyPositionType.Both}>
+              <div role="heading" aria-level={1} className={classNames.sticky}>
+                Skills
+              </div>
+            </Sticky>
+            <Skills/>
+          </ScrollablePane>
+        </div>
       </div>
     </React.Fragment>
   )
