2557b03e0eb3e65e8ca62c69e2a40eb663a689c0
Don Denoncourt <don.denoncourt@litera.com>
2023-09-25 18:47:21 -0500

    publishments open window

--- frontend/src/publishments.js
+++ frontend/src/publishments.js
@@ -1,8 +1,29 @@
 import { useState, useEffect } from "react";
+import {
+  DocumentCard,
+  DocumentCardActivity,
+  DocumentCardDetails,
+  DocumentCardPreview,
+  DocumentCardTitle,
+  IDocumentCardPreviewProps,
+  DocumentCardType,
+  IDocumentCardActivityPerson,
+} from '@fluentui/react/lib/DocumentCard';
+import { Stack, IStackTokens } from '@fluentui/react/lib/Stack';
+import { getTheme } from '@fluentui/react/lib/Styling';
 
-const Publishments=()=>{
+const stackTokens: IStackTokens = { childrenGap: 20 };
+const theme = getTheme();
+const { palette, fonts } = theme;
+
+const openDocument = (link) => { window.open(link); }
+
+const Publishments=() => {
   const publishmentsAPI = "publishments.json";
   const [publishments, setPublishments] = useState([]);
+  const previewProps: IDocumentCardPreviewProps = { previewImages: publishments };
+  /*const DocumentCardActivityPeople = [{ name: 'Annie Lindqvist', profileImageSrc: TestImages.personaFemale }];*/
+  const DocumentCardActivityPeople = [{ name: 'Annie Lindqvist', profileImageSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm9v7G3n1bvU0qWYvuetfJY1XJM0bbzo7CAn8BignF&s' }];
 
   useEffect(()=> {
     fetch(publishmentsAPI)
@@ -12,15 +33,22 @@ const Publishments=()=>{
   }, [])
 
   return(
-     <section className="Publishments">
-       <h3>Publishments</h3>
-         <ul>
-            {publishments.map((publishment) => (
-              <li key={publishment.id}>{publishment.title} {publishment.summary}</li>
-            ))}
-         </ul>
-     </section>
-  )
+    <Stack tokens={stackTokens}>
+      {publishments.map((publishment) => (
+        <DocumentCard
+          aria-label={publishment.title}
+          type={DocumentCardType.large}
+          onClick={() => openDocument(publishment.link)}
+        >
+          <DocumentCardTitle
+            title={publishment.title}
+            shouldTruncate
+          />
+            {publishment.summary}
+        </DocumentCard>
+      ))}
+    </Stack>
+  );
 }
-export default Publishments;
 
+export default Publishments;
