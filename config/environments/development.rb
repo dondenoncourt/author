51631ff8688f59bb8b1ce00bc96bd5f0fb0cf7e0
Don Denoncourt <donat@Dons-MacBook-Air.local>
2025-12-18 15:47:54 -0500

    fun with gmail SMTP

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,11 +46,14 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
+    domain: 'gmail.com',
     user_name: 'dondenoncourt@gmail.com',
     password: ENV['MAILER_PASSWORD'],
     authentication: 'plain',
-    # enable_starttls_auto: true
+    enable_starttls_auto: true
   }
+  # or use letter_opener to preview emails in browser
+  #  config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
